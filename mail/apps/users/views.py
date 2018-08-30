ae5a75773aa0a0534c0ef61f74d6bec19b58cd19
xu <aaa@aa.aaa>
2018-08-30 20:23:55 +0800

    Initial commit

---
+++ mail/apps/users/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import User
+# Create your views here.
+class ResigerUsernameCountApiview(APIView):
+    """
+    获取用户名的个数
+    GET:  /users/usernames/(?P<username>\w{5,20})/count/
+    """
+    def get(self,username,count):
+        count = User.objects.filter(username = username).count()
+        context={
+            "count":count,
+            "username":username,
+        }
+        return Response(context)
+
+
+
